8f2ee64cc08223241ddaf28512ad501793ae4fdd
Adrian Vizcaino <willbournd98@gmail.com>
2025-06-16 21:14:44 +0200

    Add files via upload
    Starting project with Spring Boot with dependencies like Spring web, Spring Data JPA, Docker Compose Support, MySQL Driver, Lombok, Spring Boot DevTools and Spring Boot Actuator.

---
+++ resolve-core-backend/resolve-core-backend/src/main/java/com/resolvecore/resolve_core_backend/ResolveCoreBackendApplication.java
@@ -0,0 +1,13 @@
+package com.resolvecore.resolve_core_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResolveCoreBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResolveCoreBackendApplication.class, args);
+	}
+
+}
